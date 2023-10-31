9e7cb0064689d0092f1a4cb9ed8baa7da059ed3a
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2023-10-31 13:27:08 -0400

    Update starterfile1.js

--- starterfile1.js
+++ starterfile1.js
@@ -123,3 +123,116 @@ export default App;
 Now, you have successfully created the components (HauntedHouse, RoomOne, RoomTwo, RoomThree, Door) 
   and added their code to your Haunted House Explorer app. Remember to make sure your project structure
     matches the file names and directories mentioned in the code. Your app is ready to run with npm start.
+
+
+
+*** Activity 2:
+
+Here are detailed instructions for students to complete the "Haunted House Activity 2." This includes where to put code, file naming conventions, directory structure, and how to run the app:
+
+Objective: In this lab, you will enhance the Haunted House Explorer app by incorporating React Hooks for state management, Redux for global state management, React Router for app navigation, and WebSockets for real-time updates.
+
+Prerequisites:
+
+Basic knowledge of React and JavaScript.
+Familiarity with Create React App and Redux is helpful but not required.
+Step-by-Step Instructions:
+Step 1: Setting Up Redux
+
+Create Redux Files:
+
+Create a new directory named redux in the src directory of your project.
+Inside the redux directory, create a file named hauntedHouseSlice.js.
+Install Redux and React Redux:
+
+Open your terminal and navigate to the project directory.
+
+Run the following command to install Redux and React Redux:
+
+bash
+Copy code
+npm install redux react-redux
+Configure Redux Store:
+
+In hauntedHouseSlice.js, create a Redux slice that defines the initial state and reducers for managing the haunted house state.
+Connect to Redux Store:
+
+In the src directory, open index.js and wrap your entire app with the Provider component, providing it with the Redux store.
+Step 2: Implementing React Hooks
+
+Refactor Components:
+
+Open the existing components (HauntedHouse.js, RoomOne.js, RoomTwo.js, RoomThree.js, Door.js) located in the src directory.
+Refactor these components to use React Hooks (e.g., useState, useEffect) for state management and side effects.
+Migrate State to Redux:
+
+Identify the relevant state variables in your components and migrate them to the Redux store.
+Update the components to use Redux for state management. Import the necessary Redux actions and selectors.
+Step 3: Implementing React Router
+
+Install React Router:
+
+Run the following command to install React Router:
+
+npm install react-router-dom
+Set Up Routes:
+
+Create a new directory named components in the src directory.
+Inside the components directory, create separate components for each room (e.g., RoomOne.js, RoomTwo.js, RoomThree.js) and configure React Router routes in a new file named AppRouter.js.
+Define routes for each room and the main HauntedHouse component in AppRouter.js.
+Update Navigation:
+
+In your Door.js component, replace the button click handlers with React Router links to navigate between rooms based on the defined routes.
+Step 4: Adding WebSockets
+
+Install WebSocket Library:
+
+Run the following command to install a WebSocket library (e.g., react-websocket):
+
+bash
+Copy code
+npm install react-websocket
+WebSocket Setup:
+
+Create a WebSocket connection to a backend server in a new file named WebSocketService.js. Implement WebSocket event listeners to handle messages and errors received from the WebSocket server.
+Update State with WebSocket Data:
+
+Modify the components (e.g., HauntedHouse.js) to update the app's state based on WebSocket updates received from the backend server.
+Step 5: Testing and Debugging
+
+Testing:
+
+Test your app thoroughly to ensure that React Hooks, Redux, React Router, and WebSockets are working as expected.
+Verify that navigation between rooms and real-time updates from the WebSocket server function correctly.
+Debugging:
+
+Use browser developer tools and Redux DevTools extension to debug any issues that may arise during development.
+Step 6: Running the App
+
+Start the Development Server:
+
+In your terminal, navigate to the project directory.
+
+Run the following command to start the development server:
+
+bash
+Copy code
+npm start
+View the App:
+
+Open your web browser and navigate to http://localhost:3000 to see your Haunted House Explorer app in action.
+Step 7: Submission
+
+Submit the Updated Code:
+
+Submit the updated code of your Haunted House Explorer app that includes React Hooks, Redux, React Router, and WebSockets integration.
+Ensure that the Redux files (hauntedHouseSlice.js) are placed in the redux directory and components are organized in the src directory.
+Include Documentation:
+
+Include a brief report or README file that explains the changes you made to the app and how each of the mentioned technologies (React Hooks, Redux, React Router, WebSockets) was implemented.
+Optionally, mention any additional features or improvements you made to the app.
+That's it! You've successfully completed the "Haunted House Activity 2" lab, incorporating advanced features into your Haunted House Explorer app.
+
+
+
+
